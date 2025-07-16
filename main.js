7937fb6e868505d2c94092694d4bcbc1b65acf19
Mustopha200 <mustophaadesinamyd@gmail.com>
2025-07-16 10:53:07 +0100

    javascript class

---
+++ main.js
@@ -0,0 +1,57 @@
+function myproduct(toffa){
+    if(toffa == "a"){
+        return "axe"
+    }else if(toffa == "b"){
+        return "ball"
+    }else if(toffa == "c"){
+        return "card"
+    }else if(toffa == "d"){
+        return "dog"
+    }else if(toffa == "e"){
+        return "egg"
+    }else if(toffa == "f"){
+        return "fish"
+    }else if(toffa == "g"){
+        return "gun"
+    }else if(toffa == "h"){
+        return "hoe"
+    }else if(toffa == "i"){
+        return "ink"
+    }else if(toffa == "j"){
+        return "jug"
+    }else if(toffa == "k"){
+        return "kit"
+    }else if(toffa == "l"){
+        return "lamp"
+    }else if(toffa == "m"){
+        return "mat"
+    }else if(toffa == "n"){
+        return "not"
+    }else if(toffa == "o"){
+        return "orange"
+    }else if(toffa == "p"){
+        return "pad"
+    }else if(toffa == "q"){
+        return "query"
+    }else if(toffa == "r"){
+        return "rat"
+    }else if(toffa == "s"){
+        return "snake"
+    }else if(toffa == "t"){
+        return "turn"
+    }else if(toffa == "u"){
+        return "umbrella"
+    }else if(toffa == "v"){
+        return "pot"
+    }else if(toffa == "w"){
+        return "rat"
+    }else if(toffa == "x"){
+        return "xam"
+    }else if(toffa == "y"){
+        return "yam"
+    }else if(toffa == "z"){
+        return "zebra"
+    }else{
+        return "product not found"
+    }
+}
